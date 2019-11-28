015b0d35fb007522efae4384401eceb8276d0223
ChitogeOwO <56685381+ChitogeOwO@users.noreply.github.com>
2019-11-28 12:28:42 +0530

    Update Avatar.py

--- cogs/Avatar.py
+++ cogs/Avatar.py
@@ -11,15 +11,13 @@ class avatar(commands.Cog, name = "Avatar"):
         print('Cogs are loaded for avatar')
 
     @commands.command()
-    async def avatar(self, ctx, member: discord.Member):
+    async def avatar(self, ctx, member: discord.Member = None):
+        member = ctx.author if not member else member
         Savatar = discord.Embed(color = discord.Color.default())
         Savatar = embed = discord.Embed(colour=member.color)
         Savatar = embed.set_author(name=f"Avatar - {member}")
         Savatar = embed.set_footer(text=f"requested by {ctx.author}", icon_url=ctx.author.avatar_url)
         Savatar.set_image(url='{}'.format(member.avatar_url))
-        """embed = discord.Embed(colour=member.color)
-        embed.set_author(name=f"Avatar - {member}")
-        embed.set_footer(text=f"requested by {ctx.author}", icon_url=ctx.author.avatar_url)"""
 
         await ctx.send(embed= Savatar)
 
