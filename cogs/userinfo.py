3486483defc8c1b525c801497920004eeb1a8845
ChitogeOwO <56685381+ChitogeOwO@users.noreply.github.com>
2021-03-04 21:53:36 +0530

    Update userinfo.py

--- cogs/userinfo.py
+++ cogs/userinfo.py
@@ -12,12 +12,10 @@ class userinfo(commands.Cog, name="Userinfo"):
     async def on_ready(self):
         print('cogs loaded for userinfo')
 
-
     @commands.command()
     async def userinfo(self , ctx, member: discord.Member = None):
         member = ctx.author if not member else member
         roles = [role for role in member.roles]
-
 
         embed = discord.Embed(colour=member.color, timestamp=ctx.message.created_at)
 
@@ -27,19 +25,15 @@ class userinfo(commands.Cog, name="Userinfo"):
 
         embed.add_field(name="ID:", value=member.id)
         embed.add_field(name="Status:", value = member.status)
-        embed.add_field(name="Server name:", value=member.display_name, inline=False)
-        
+        embed.add_field(name="User name:", value=member.display_name, inline=False)
 
         embed.add_field(name="Created at:", value=member.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"),inline=False)
         embed.add_field(name="Joined at:", value=member.joined_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"),inline=False)
 
         embed.add_field(name="Top Role:", value=member.top_role.mention, inline=False)
-        embed.add_field(name=f"Roles ({len(roles)})", value="".join([role.mention for role in roles]), inline=False)
+        embed.add_field(name=f"Roles ({len(roles)})", value="".join(sorted([role.mention for role in roles])), inline=False)
         
         await ctx.send(embed=embed)
-
-
-
 
 def setup(client):
     client.add_cog(userinfo(client))
